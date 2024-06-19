8321cea52ba06039b31b15069c3d95e66c0f63f5
Bruno Monteiro <bruno.monteiro@voalle.com.br>
2024-06-19 11:56:33 -0300

    Add uvicorn log configuration and documentation to code

--- lib/libmessage.py
+++ lib/libmessage.py
@@ -1,7 +1,22 @@
+# Copyright (c) 2024, Bruno Monteiro
+# All rights reserved.
+"""
+Library to manage messages and log.
+"""
+
 from lib.liblog import *
 log = liblog()
 
 def default_invalid_field(field):
+  """
+  Default message for invalid field.
+
+  Args:
+      field (str): The name of the invalid field.
+
+  Returns:
+      str: The default message.
+  """
   m = f"field {field} is invalid, please try again."
   log.critical(m)
   return m
